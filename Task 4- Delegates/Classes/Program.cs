76267ffddb565691d69e67d54dc5bb91f2a457b6
LivintLucian <48414867+LivintLucian@users.noreply.github.com>
2020-07-20 15:14:57 +0300

    Add files via upload

---
+++ Task 4- Delegates/Classes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Filter<T>(List<T> strings, Func<T, bool> condition)
+        {
+            var returnList = strings.Where(condition);
+
+            foreach (var entry in returnList)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+        static void PrintOddNumbers(int n)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write((i * 2) + 1);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            long average = 0;
+            Action<int> printAction = PrintOddNumbers;
+            
+            for (int index = 0; index < 100; index++)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                //printAction(10);
+                watch.Stop();
+                var elapsedMS = watch.ElapsedMilliseconds;
+                average += elapsedMS;
+                //Console.WriteLine("\nTime- " + index + ": " + elapsedMS);
+            }
+
+            //Console.WriteLine(average / 100);
+            /*
+             * Ex 2.
+             */
+            List<string> list=new List<string>(){"aa","bb","c","d","eee"};
+            Filter(list, s => s.Length == 2);
+        }
+    }
+}
