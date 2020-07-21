2dd5d763bf9f47732ed7e4c38776b1744cb299fa
LivintLucian <48414867+LivintLucian@users.noreply.github.com>
2020-07-21 16:27:42 +0300

    Add files via upload

---
+++ Task 5- Exceptions & Async/Classes/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program objProgram = new Program();
+            objProgram.ReportTime();
+            Console.Write("ASync Time: ");
+            objProgram.ReportTimeAsync();
+        }
+
+        public List<Provider> InitProviders()
+        {
+            var providers = new List<Provider>();
+
+            providers.Add(Provider.CreateProvider(1, "Busu", 35));
+            providers.Add(Provider.CreateProvider(1, "Ioan", 29));
+            providers.Add(Provider.CreateProvider(1, "Sebastian", 31));
+            providers.Add(Provider.CreateProvider(1, "Stefan", 28));
+            providers.Add(Provider.CreateProvider(1, "Alexandru", 27));
+
+            return providers;
+        }
+
+        public void GetTemperature()
+        {
+            List<Provider> providersTemp = InitProviders();
+            foreach (var entry in providersTemp)
+            {
+                var result = entry.GetTemperature();
+            }
+        }
+
+        public async Task<long> GetTemperatureAsync()
+        {
+            List<Provider> providersTemp = InitProviders();
+
+            List<Task<int>> providersASync = new List<Task<int>>();
+
+            foreach (var entry in providersTemp)
+            {
+                //var result = await Task.Run(() => entry.Temperature);
+                providersASync.Add(Task.Run(() => entry.GetTemperature()));
+            }
+
+            var results = await Task.WhenAll(providersASync);
+            return 1;
+        }
+        public long Timer()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            GetTemperature();
+            watch.Stop();
+            var timeMS = watch.ElapsedMilliseconds;
+
+            return timeMS;
+        }
+
+        public async Task<long> TimerAsync()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var result = await GetTemperatureAsync();
+            watch.Stop();
+            var timeMS = watch.ElapsedMilliseconds;
+
+            Console.WriteLine(timeMS);
+            return 1;
+        }
+
+        public void ReportTime()
+        {
+            Console.WriteLine("Sync time: " + Timer());
+        }
+
+        public void ReportTimeAsync()
+        {
+            TimerAsync().Wait();
+        }
+    }
+}
+
+//Aplicatie de meteo- Lista de prognoze meteo, care-ti trimit temperatura pe maine. Temperatura( media temp primite de la providerii)
+//Varianta in care iei toate prognozele si le faci media si returnezi
+//Varianta rapida, asteapta primele 3 rezultate, face media si returneaza ( restul 7 sunt ignorante ).
+//In cazul in care gasiti o solutie, tratati exceptiile.
+// *Calculele in paralel
---
+++ Task 5- Exceptions & Async/Classes/Provider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Async
+{
+    class Provider
+    {
+        public static Provider CreateProvider(int _id, string _name , int _temperature)
+        {
+            return new Provider()
+            {
+                Id = _id,
+                Name = _name,
+                Temperature = _temperature,
+            };
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Temperature { get; set; }
+
+        public int GetTemperature()
+        {
+            Thread.Sleep(1000);
+            return Temperature;
+        }
+    }
+}
