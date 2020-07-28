f9af03993edf627df8132b1c16d759f6c0dd75f6
LivintLucian <48414867+LivintLucian@users.noreply.github.com>
2020-07-28 16:06:03 +0300

    Add files via upload

---
+++ Task 10- DependencyInjection/Classes/Factory.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectFactory objectFactory = new ObjectFactory();
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Id", 1);
+            Armour armour = objectFactory.Get(ObjectType.Armour, data) as Armour;
+            Weapon weapon = objectFactory.Get(ObjectType.Weapon, data) as Weapon;
+
+            Console.WriteLine(armour.GetInfo());
+            Console.WriteLine(weapon.GetInfo());
+        }
+    }
+
+    enum ObjectType
+    {
+        Armour,
+        Weapon
+    }
+    //class factory
+    interface IObject
+    {
+        String GetInfo();
+    }
+
+    class ObjectFactory
+    {
+        private ObjectType _type;
+        private Dictionary<string, object> _data;
+        public IObject Get(ObjectType type, Dictionary<string, object> data)
+        {
+            _type = type;
+            _data = data;
+
+            return GetObject();
+        }
+
+        private IObject GetObject()
+        {
+            IObject obj = null;
+
+            Int32 id = Convert.ToInt32(_data["Id"]);
+
+            //fetch the object information based on the supplied type and data
+            switch (_type)
+            {
+                case ObjectType.Armour:
+                    if (id == 1)
+                    {
+                        obj = new Armour()
+                        {
+                            Id = 1, Name = "Shield of Sunlight", DefenceRating = 5, Weight = 67, Type = "Shield"
+                        };
+                    }
+
+                    break;
+
+                case ObjectType.Weapon:
+                    if (id == 1)
+                    {
+                        obj = new Weapon()
+                        {
+                            Id = 1, Name = "Sword Of Flames", DamageRating = 12, Weight = 67, Type = "One-Handed Sword"
+                        };
+                    }
+
+                    break;
+
+                default:
+                    obj = null;
+                    break;
+            }
+
+            return obj;
+        }
+    }
+    class Armour:IObject
+    {
+        public Int32 Id;
+        public String Name;
+        public Int32 DefenceRating;
+        public Int32 Weight;
+        public String Type;
+
+        public virtual String GetInfo()
+        {
+            return String.Format("Armour name is {0}", Name);
+        }
+    }
+
+    class Weapon:IObject
+    {
+        public Int32 Id;
+        public String Name;
+        public Int32 DamageRating;
+        public Int32 Weight;
+        public String Type;
+
+        public virtual String GetInfo()
+        {
+            return String.Format("Weapon name is {0}", Name);
+        }
+    }
+}
---
+++ Task 10- DependencyInjection/Classes/SingleTon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace DependencyInjection
+{
+    public class SingleTon
+    {
+        private static readonly SingleTon _instance = new SingleTon();
+        private List<int> numbers =new List<int>();
+        private int nextNumber = 0;
+        private SingleTon()
+        {
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+        }
+
+        public static SingleTon GetNumbers()
+        {
+            return _instance;
+        }
+        public int GetNextNumber()
+        {
+            int output = numbers[nextNumber];
+
+            nextNumber += 1;
+
+            if (nextNumber >= numbers.Count)
+            {
+                nextNumber = 0;
+            }
+
+
+            return output;
+        }
+    }
+}
