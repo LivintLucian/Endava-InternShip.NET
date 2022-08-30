cabd8184bb41e5aa6978d5f8ad8a77d337df11d4
LivintLucian <livintlucian@gmail.com>
2022-08-30 10:32:32 +0300

    - Test githooks

--- Task 3- Web Api/Classes/Server/Startup.cs
+++ Task 3- Web Api/Classes/Server/Startup.cs
@@ -22,7 +22,7 @@ namespace Server
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
+            // specifying the Swagger JSON endpoint...
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
